87a6977a5544bebe8a2b4084e5391efd0e0a1a5d
DEVCOMP\erymn <erymnur@gmail.com>
2026-04-17 14:48:17 +0700

    Interpolation introduction

--- my-angular-app/src/main.ts
+++ my-angular-app/src/main.ts
@@ -6,18 +6,36 @@ import { Component } from '@angular/core';
 // bootstrapApplication(App, appConfig)
 //   .catch((err) => console.error(err));
 
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   // template: `<h1>Hello {{name}}</h1>`
+//   template: `
+//     <h3>Hello {{name}}</h3>
+//     <button (click)="name = 'Angular 21'">Reset</button>
+//   `
+// })
+
+// class App{ 
+//   name = 'World'
+// }
+
+//Interpolation in Angular
 @Component({
   selector: 'app-root',
   standalone: true,
   // template: `<h1>Hello {{name}}</h1>`
   template: `
-    <h3>Hello {{name}}</h3>
-    <button (click)="name = 'Angular 21'">Reset</button>
+    <h3>{{title}}</h3>
+    <p>Hello {{name}}</p>
+    <p>2 + 3 = {{2+3}}</p>
+    <p>Upper: {{name.toUpperCase()}}</p>
   `
 })
 
 class App{ 
-  name = 'World'
+  title = 'Template and Interpolation in Angular'
+  name = 'Angular'
 }
 
 bootstrapApplication(App);
